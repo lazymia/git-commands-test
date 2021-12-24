5c951f32c15423636ecb5451d4008b09fc7f8936
chiamaka osuji <hello.chiamaka@gmail.com>
2021-12-24 19:00:08 +0100

    first commit

---
+++ dodo.js
@@ -0,0 +1,19 @@
+
+function buttonChange(){
+    document.getElementById('change').innerHTML = data;
+} 
+
+let data = {
+    name: 'chiamaka osuji',
+    age: 27,
+    profession: 'tech sis',
+    location: 'lagos'
+}
+document.write(data)
+ 
+let Myname = 'chiamaka';
+let age = 27
+
+function myName(){
+    document.getElementById('myname').innerHTML = 'my name is' + Myname  + ' and I am ' + age + 'years old'
+}
